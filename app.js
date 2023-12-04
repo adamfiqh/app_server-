22276a3c6e567009ff1a6c84f80af7b23457d0e6
adamfiqh <adamfr24@gmail.com>
2023-12-04 10:46:56 +0700

    baru pagi inib

--- app.js
+++ app.js
@@ -4,24 +4,26 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const cors = require("cors");
+
 var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-app.use(cors());
+
+app.use(cors);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-//home
+// home
 app.use("/", function (req, res) {
   res.render("index", {
-    title: "Eduwork API Service",
+    title: "eduwork api service",
   });
 });
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const { dbHost, dbPass, dbName, dbPort, dbUser } = require("../app/config");
 mongoose.connect(
-  `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`
+  `mongodb://eduwork:12345@localhost:27017/eduworkstore?authSource=admin`
 );
 const db = mongoose.connection;
 
